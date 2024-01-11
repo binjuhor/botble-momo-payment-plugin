1696c5f1a3e0adcf01be925613fe1e5feab009c4
Hoang Kiem <kiemhd@outlook.com>
2024-01-11 11:08:28 +0700

    fix: remove data after uninstall

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,12 +11,14 @@ class Plugin extends PluginOperationAbstract
     {
         Setting::query()
             ->whereIn('key', [
-                'payment_momo_name',
-                'payment_momo_description',
-                'payment_momo_client_id',
-                'payment_momo_client_secret',
-                'payment_momo_mode',
-                'payment_momo_status',
+                'payment_via_momo',
+                'momo_description',
+                'after_service_registration_msg',
+                'enter_partner_code_and_secret',
+                'momo_access_key',
+                'momo_secret_key',
+                'momo_public_key',
+                'momo_mode',
             ])
             ->delete();
     }
